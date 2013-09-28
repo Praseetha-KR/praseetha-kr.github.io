eba65a11536450bcd819243f862b4e1dd5af3c47
Praseetha-KR <praseetha@hasgeek.com>
2013-09-29 00:06:02 +0530

    Moved script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-
+$(function(){
+  var headerHeight = $('header').height();
+  $(window).scroll(function(){
+    var scrollArea = $(this).scrollTop();
+    var trigger = headerHeight - scrollArea;
+    if(trigger > 40){
+      $('header').height(trigger);
+      $('header').css({lineHeight : trigger+'px'});
+    }
+  }).scroll();
+});
